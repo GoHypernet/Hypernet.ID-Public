71596f5d65b4f2ae6fdc7ccee7937a18b46eff67
TtheBC01 <todd@hypernetlabs.io>
2021-12-12 01:49:03 +0000

    update to contracts README and Test.sol

--- packages/contracts-integration/contracts/Test.sol
+++ packages/contracts-integration/contracts/Test.sol
@@ -4,16 +4,17 @@ pragma solidity ^0.8.0;
 import "./ID.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @dev this is an contract used for demonstrating how to use the on-chain Hypernet.ID
+/// @dev this is a simple smart contract demonstrating how to use the on-chain Hypernet.ID
 /// registry to protect your contract via the modifiers defined in ID.sol. Running the
-/// Hardhat tests will give you an accurate estimate of the additional gas requirements
-/// for enforcing id verification.
+/// Hardhat tests in this subpackage will give you an accurate estimate of the additional
+/// gas requirements for enforcing id verification.
 contract Test is Ownable, ID {
 
     string public name;
 
     constructor (string memory _name, string memory _CRITERIA) {
         name = _name;
+		// CRITERIA is declared as a public bytes8 type in ID.sol
         CRITERIA = _fromTokenURIToBytes8(_CRITERIA);
     }
 
@@ -30,6 +31,8 @@ contract Test is Ownable, ID {
 	external
 	onlyOwner()
 	{
+		// registryAddress is declared as a public address type in ID.sol and
+		// its default value is address(0)
         registryAddress = _registryAddress;
     }
 
@@ -49,6 +52,7 @@ contract Test is Ownable, ID {
     }
 
     /// @dev a function that can only be called if user has undergone a specified identification process
+	/// defined by CRITERIA
     function changeNameIfVerifiedWithCriteria(string memory _name)
     external
     onlyVerifiedWithCriteria()
@@ -57,7 +61,7 @@ contract Test is Ownable, ID {
     }
 
 	/// @dev a function that can only be called if user has undergone a specified identification process
-	/// and specify their tokenid as one of the function inputs
+	/// defined by CRITERIA. Additionally, the user must specify their tokenid as one of the function inputs.
     function changeNameIfVerifiedTokenWithCriteria(string memory _name, uint256 tokenid)
     external
     onlyVerifiedTokenWithCriteria(tokenid)
